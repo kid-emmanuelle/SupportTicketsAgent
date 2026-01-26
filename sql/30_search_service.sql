743a5a70b977d6db1a195b08f6a61543ccab4236
jewin.cheng <jewin.cheng@gmail.com>
2026-01-26 13:47:28 +0100

    search_service: added init curated table creation, insert and search service creation. Notebook build search using to test

--- sql/30_search_service.sql
+++ sql/30_search_service.sql
@@ -14,7 +14,20 @@ CREATE CORTEX SEARCH SERVICE support_tickets_search_service
 ON chunk_text
 ATTRIBUTES language, product, topic, kb_id
 FROM CURATED.KB_CHUNKS;
+
 */
+
+CREATE OR REPLACE CORTEX SEARCH SERVICE support_tickets_search_service
+  ON body_answer
+  ATTRIBUTES subject, priority, type, queue, language
+  WAREHOUSE = compute_wh
+  TARGET_LAG = '1 day'
+  EMBEDDING_MODEL = 'snowflake-arctic-embed-l-v2.0'
+  AS (
+    SELECT *,
+       concat('Body: ', body, ' \n Answer:', answer) as body_answer
+    FROM CURATED.TICKETS
+);
 
 -- Alternative: Search on KB articles directly (if no chunking)
 /*
